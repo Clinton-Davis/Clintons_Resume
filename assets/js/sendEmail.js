ad703fd1f96df3ceae2e8e731303fec521278809
Clinton Davis <you@example.com>
2020-01-17 17:13:43 +0000

    added mail.js and code but having issue will look at later

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+	emailjs
+		.send("gmail", "code_resume", {
+			from_name: contactForm.name.value,
+			from_email: contactForm.emailaddress.value,
+			project_request: contactForm.projectsummary.value
+		})
+		.then(
+			function(responce) {
+				console.log("SUCCESS", responce);
+			},
+			function(error) {
+				console.log("FAILED", error);
+			}
+		);
+}
